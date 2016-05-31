6f1e3007edaf65a6c7cd2b41e0f4d0f88779104c
memo <me@memo.blue>
2016-05-31 11:50:44 -0400

    initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+var { Link, IndexLink } = require('react-router');
+
+var Nav = () => {
+  return (
+    <div className="top-bar nav">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Timer App</li>
+          <li><IndexLink activeClassName="active" to="/">Timer</IndexLink></li>
+          <li><Link activeClassName="active" to="/">Countdown</Link></li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">Fork me on <a target="_blank" href="https://github.com/memoblue/reactTimer">GitHub</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Nav;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({ // make jquery available in all components
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname, // current dir in node
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+        Main: 'app/components/Main.jsx',
+        Nav: 'app/components/Nav.jsx',
+        applicationStyles: 'app/styles/app.scss'
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader', // convert jsx to es5 code
+        query: {
+          presets: ['react', 'es2015', 'stage-0'] // what to load
+        },
+        test: /\.jsx?$/, // which files to get
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
