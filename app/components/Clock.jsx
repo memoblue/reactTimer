c641f2fc9075172e746c43a4be170e012955dc42
memo <me@memo.blue>
2016-06-04 18:15:44 -0400

    added Clock component (static for now) and its styles

---
+++ app/components/Clock.jsx
@@ -0,0 +1,36 @@
+var React = require('react');
+
+var Clock = React.createClass({
+  getDefaultProps: function() {
+    totalSeconds: 0
+  },
+  propTypes: {
+    totalSeconds: React.PropTypes.number
+  },
+  formatSeconds: function(totalSeconds) {
+    var minutes = Math.floor(totalSeconds / 60);
+    var seconds = totalSeconds % 60;
+
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    return `${minutes}:${seconds}`;
+  },
+  render: function() {
+    var { totalSeconds } = this.props;
+    return (
+      <div className="clock">
+        <span className="clock__text">
+          {this.formatSeconds(totalSeconds)}
+        </span>
+      </div>
+    );
+  }
+});
+
+module.exports = Clock;
