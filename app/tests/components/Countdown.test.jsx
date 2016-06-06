91ffa7f5881c798e1b1a16493fe0e52e06c8aa71
memo <me@memo.blue>
2016-06-05 22:26:44 -0400

    add countdown functionality

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,37 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jQuery');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+  it('should exist', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCountdown', () => {
+    it('should set state to "started" and countdown', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done(); // mocha doesn't support async tests by default (arg line 15 too)
+      }, 1001);
+    })
+
+    it('should never countdown to less than 0', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(1);
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done();
+      }, 3001);
+    })
+  });
+});
